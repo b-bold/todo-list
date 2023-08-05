fc4e2d361fc18c92a5bd9340fdf68877c8357aff
Neka Kerr <anneika.kerr@gmail.com>
2023-08-05 18:57:31 -0400

    added validations. not pretty

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,36 @@
 import React, { useState } from "react"
 import { Welcome } from './Welcome'
+// import "./Form.css"
 
 export const Login = (props) => {
     const [email, setEmail ] = useState('');
     const [pass, setPass ] = useState('');
+    const [error, setError] = useState(false);
     const [page, setPage] = useState(false)
 
+    const handleInputChange = (event) => {
+        if (event.target.name === 'email') {
+            setEmail(event.target.value)
+        }
+
+        if (event.target.name === 'password') {
+            setPass(event.target.value)
+        }
+
+        if (email.length === 0 || pass.length === 0) {
+            setError(true)   
+        } else {
+            setError(false)
+        }
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault(); // Prevent default form submission behavior
-        setPage(true);
-    // setPass()
-    // setEmail()
+        if (error) {
+            return setPage(false)
+        } else {
+            setPage(true)
+        }
     }
     
     return (
@@ -20,10 +39,15 @@ export const Login = (props) => {
                 <div className="auth-form-container">
                     <h2> Login </h2>
                     <form className="login-form" onSubmit={handleSubmit}>
-                        <label htmlFor="email">Email </label>
-                        <input value={email} type="email" placeholder="youremail@gmail.com" name="email"/>
-                        <label htmlFor="password">Password </label>
-                        <input value={pass} type="password" placeholder="*****" name="password" />
+                        {error && email.length <= 0? 
+                            <label htmlFor="email"> Email cannot be empty </label> : '' }
+                            <input value={email} type="email" placeholder="youremail@gmail.com" name="email" onChange={handleInputChange}/>
+
+                        {error && pass.length <= 0 ? 
+                        <label htmlFor="password">Password cannot be empty </label>: '' }
+
+                        <input value={pass} type="password" placeholder="*****" name="password" onChange={handleInputChange}/>
+
                         <button className='login-btn'>Log In</button>
                     </form>   
                     <button className='register-btn' onClick={() => props.onFormSwitch('register')}>Register Here </button>
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,16 +1,55 @@
-import React, { useState } from "react"
+import React, { ChangeEvent, useState } from "react"
 import { Welcome } from './Welcome'
 
 export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
-    const[page, setPage] = useState(false)
+    const [age, setAge] = useState('')
+    const [error, setError] = useState(false);
+    const [page, setPage] = useState(false)
 
+    const handleInputChange = (event) => {
+        // does this code work? yes
+        // does this code seem ugly? also yes.
+        if (event.target.name === 'name'){
+            setName(event.target.value)
+        } 
+        
+        if (event.target.name === 'email'){
+            setEmail(event.target.value)
+        }
+        
+        if (event.target.name === 'password'){
+            setPass(event.target.value) 
+        } 
+        
+        if (event.target.name === 'age'){
+            setAge(event.target.value)
+        }
+
+        if (name.length === 0) {
+            setError(true)
+        } else if (pass.length === 0) {
+            setError(true)
+        } else if (age.length === 0) {
+            setError(true)
+        } else if (age.length === 0) {
+            setError(true)
+        } else if (email.length === 0) {
+            setError(true)
+        } else {
+            setError(false)
+        }
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault(); // Prevent default form submission behavior
-        setPage(true);
+        if (error) {
+            return setPage(false)
+        } else {
+            setPage(true)
+        }
     }
 
     return (
@@ -19,12 +58,22 @@ export const Register = (props) => {
                 <div className="auth-form-container"> 
                     <h2> Register </h2>  
                     <form className="register-form" onSubmit={handleSubmit}>
-                        <label>Full Name </label>
-                        <input value={name} name="name" id="id" placeholder="full name" />
-                        <label htmlFor="email">Email </label>
-                        <input value={email} type="email" placeholder="youremail@gmail.com" name="email" />
-                        <label htmlFor="password">Password </label>
-                        <input value={pass} type="password" placeholder="*****" name="password" />
+                        {error && name.length <= 0 ? 
+                            <label>Full Name cannot be empty</label> : ''}
+                            <input value={name} name="name" id="id" placeholder="full name" onChange={handleInputChange} />
+                        
+                        {error && email.length <= 0 ? 
+                            <label htmlFor="email">Email </label> : ''}
+                            <input value={email} type="email" placeholder="youremail@gmail.com" name="email" onChange={handleInputChange} />
+                        
+                        {error && pass.length <= 0 ? 
+                            <label htmlFor="password">Password cannot be empty</label> : '' }
+                            <input value={pass} type="password" placeholder="*****" name="password" onChange={handleInputChange} />
+                        
+                        {error && age.length <= 0 ? 
+                            <label htmlFor="age">Age </label> : '' }
+                            <input value={age} type="age" placeholder="*****" name="age" onChange={handleInputChange} />
+                        
                         <button className='login-btn'>Log In</button>
                     </form>
                     <button className='register-btn' onClick={() => props.onFormSwitch('login')}>Login Here </button>
